d1ab742bee441d83767bda61e278f11fecad6f1c
kiran <kiran@DESKTOP-MD6S2SS>
2019-05-22 11:09:42 +1200

    Add project files.

---
+++ Test data/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-1 + 4 * 6);
+            //Expected result will be 23
+            Console.WriteLine((35 + 5) % 7);
+            //Expected result will be 5
+            Console.WriteLine(14 + -4 * 6 / 11);
+            //Expected result will be 12
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+            //Expected result will be 3
+        }
+    }
+}
